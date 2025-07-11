ae276a36641306e4d64e8cdc7eb23cd9bbb97e57
Giovanni Vendola <giovannivendola87@gmail.com>
2025-07-11 10:53:27 +0200

    ho riportato le modifiche che sono state fatte dai miei colleghi in questo branch per poter lavorare in maniera corretta.

---
+++ adventure/src/main/java/it/tutta/colpa/del/caffe/adventure/entity/Dialogo.java
@@ -0,0 +1,36 @@
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package it.tutta.colpa.del.caffe.adventure.entity;
+
+import it.tutta.colpa.del.caffe.adventure.utility.StringArcoGrafo;
+import org.jgrapht.graph.DefaultDirectedGraph;
+import org.jgrapht.Graph;
+
+/**
+ *
+ * @author giova
+ */
+public class Dialogo  {
+    
+    private final Graph <String,StringArcoGrafo> dialogo;
+    
+    public Dialogo(){
+        
+        dialogo= new DefaultDirectedGraph<>(StringArcoGrafo.class);
+    }
+    
+    public void addDialogo(String dialogo){
+        this.dialogo.addVertex(dialogo);
+    }
+    
+    public void addRisposta(String domandaP, String domandaA,String risposta){
+        this.dialogo.addEdge(domandaP, domandaA, new StringArcoGrafo(risposta));
+    }
+   // get da fare 
+    
+    
+
+}
