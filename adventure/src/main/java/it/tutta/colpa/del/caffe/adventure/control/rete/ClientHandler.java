53c594080050943f9e8cade939244f94203b5885
giovav <giovannivittore@icloud.com>
2025-07-11 10:32:39 +0200

    Ho reso il server multiclient.

---
+++ adventure/src/main/java/it/tutta/colpa/del/caffe/adventure/control/rete/ClientHandler.java
@@ -0,0 +1,51 @@
+package it.tutta.colpa.del.caffe.adventure.control.rete;
+
+import it.tutta.colpa.del.caffe.adventure.other.GestioneDB;
+import java.io.*;
+import java.net.Socket;
+import java.sql.SQLException;
+
+public class ClientHandler extends Thread {
+
+    private final Socket clientSocket;
+
+    public ClientHandler(Socket socket) {
+        this.clientSocket = socket;
+        System.out.println("Nuovo client connesso: " + clientSocket.getInetAddress());
+    }
+
+    @Override
+    public void run() {
+        try (
+                this.clientSocket;
+                ObjectOutputStream out = new ObjectOutputStream(clientSocket.getOutputStream());
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))
+        ) {
+            GestioneDB dataBase = new GestioneDB();
+            String richiesta;
+            while ((richiesta = in.readLine()) != null) {
+                System.out.println("Richiesta da " + clientSocket.getInetAddress() + ": " + richiesta);
+
+                if (richiesta.equals("comandi")) {
+                    out.writeObject(dataBase.getComandi());
+                } else if (richiesta.equals("mappa")) {
+                    out.writeObject(dataBase.creaMappa());
+                } else if (richiesta.startsWith("descrizione-aggiornata-")) {
+                    String[] tk = richiesta.split("-");
+                    out.writeObject(dataBase.aggiornaDialoghi(Integer.parseInt(tk[2])));
+                } else if (richiesta.startsWith("oggetto-")) {
+                    String[] tk = richiesta.split("-");
+                } else {
+                    out.writeObject("Errore: Comando non riconosciuto");
+                }
+            }
+
+        } catch (IOException e) {
+            System.err.println("Errore di comunicazione con il client: " + e.getMessage());
+        } catch (SQLException e) {
+            System.err.println("Errore database durante la gestione del client: " + e.getMessage());
+        }
+
+        System.out.println("Connessione con " + clientSocket.getInetAddress() + " terminata.");
+    }
+}
--- adventure/src/main/java/it/tutta/colpa/del/caffe/adventure/control/rete/Server.java
+++ adventure/src/main/java/it/tutta/colpa/del/caffe/adventure/control/rete/Server.java
@@ -1,58 +1,29 @@
 package it.tutta.colpa.del.caffe.adventure.control.rete;
-import java.io.*;
+
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.sql.SQLException;
-
-import it.tutta.colpa.del.caffe.adventure.other.GestioneDB;
-
 
 public class Server {
 
-    private ServerSocket serverSocket;
-    private final int port=49152;
+    private final int port = 49152;
 
-    public Server() {
-        Socket socket = null;
-        try {
-            serverSocket = new ServerSocket(port);
+    public void start() {
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
             System.out.println("Server in ascolto sulla porta " + port);
 
-            socket = serverSocket.accept(); // accetta la connessione dal client
-            System.out.println("Connessione accettata da " + socket.getInetAddress());
-            GestioneDB dataBase = new GestioneDB();
-            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
-
             while (true) {
-                String richiesta = in.readLine();
-                if (richiesta.equals("comandi")) {
-                    out.writeObject(dataBase.getComandi());
-                }else if (richiesta.equals("mappa")){
-                    out.writeObject(dataBase.creaMappa());
-                } else if (richiesta.contains("descrizione-aggiornata-")) {
-                    String[] tk = richiesta.split("-");
-                    out.writeObject(dataBase.aggiornaDialoghi(Integer.parseInt(tk[2])));
-                }else if (richiesta.contains("oggetto-")) {
-                    String[] tk = richiesta.split("-");
-                    //out.writeObject(dataBase.(Integer.parseInt(tk[1])));
-
-                }else if (richiesta.equals("fine")) {
-                    break;      // chiudo la connessione
-                } else {
-                    out.writeObject(new Exception("Non trovato"));
-                }
+                Socket clientSocket = serverSocket.accept();
+                new ClientHandler(clientSocket).start();
             }
-
-        } catch (IOException | SQLException e) {
+        } catch (IOException e) {
+            System.err.println("Errore nell'avvio del server: " + e.getMessage());
             e.printStackTrace();
-            } finally {
-            try {
-                if (socket != null) socket.close();
-                if (serverSocket != null) serverSocket.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
         }
+    }
+
+    public static void main(String[] args) {
+        Server server = new Server();
+        server.start();
     }
 }
