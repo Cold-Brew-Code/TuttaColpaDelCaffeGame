976f199e042bd4c68e672c4982fb5a516dc7662b
giovav <giovannivittore@icloud.com>
2025-07-14 16:32:19 +0200

    Feature: termina connessione.
    Aggiunta la parte di codice che permette al client di terminare la comunicazione con il server.

--- adventure/src/main/java/it/tutta/colpa/del/caffe/rete/ClientHandler.java
+++ adventure/src/main/java/it/tutta/colpa/del/caffe/rete/ClientHandler.java
@@ -4,13 +4,15 @@ import java.io.*;
 import java.net.Socket;
 import java.sql.SQLException;
 
+/**
+ * @author giovav
+ */
 public class ClientHandler extends Thread {
 
     private final Socket clientSocket;
-
     public ClientHandler(Socket socket) {
         this.clientSocket = socket;
-        System.out.println("Nuovo client connesso: " + clientSocket.getInetAddress());
+        System.out.println("[Debug rete/ClientHandler] Nuovo client connesso: " + clientSocket.getInetAddress());
     }
 
     @Override
@@ -22,28 +24,41 @@ public class ClientHandler extends Thread {
         ) {
             DataBaseManager dataBase = new DataBaseManager();
             String richiesta;
-            while ((richiesta = in.readLine()) != null) {
-                System.out.println("[Debug rete/ClientHandler]Richiesta da " + clientSocket.getInetAddress() + ": " + richiesta);
-
+            while (true) {
+                richiesta = in.readLine();
+                System.out.println("[Debug rete/ClientHandler] Richiesta da " + clientSocket.getInetAddress() + ": " + richiesta);
                 if (richiesta.equals("comandi")) {
                     out.writeObject(dataBase.askForCommands());
                 } else if (richiesta.equals("mappa")) {
                     out.writeObject(dataBase.askForGameMap());
                 } else if (richiesta.startsWith("descrizione-aggiornata-")) {
-                    String[] tk = richiesta.split("-");
-                    out.writeObject(dataBase.askForNewRoomLook(Integer.parseInt(tk[2])));
+                    try {
+                        String[] tk = richiesta.split("-");
+                        out.writeObject(dataBase.askForNewRoomLook(Integer.parseInt(tk[2])));
+                    } catch (Exception e) {
+                        out.writeObject("Oops, qualcosa è andato storto!");
+                    }
                 } else if (richiesta.startsWith("oggetto-")) {
-                    String[] tk = richiesta.split("-");
-                    out.writeObject(dataBase.askForItem(Integer.parseInt(tk[1])));
+                    try {
+                        String[] tk = richiesta.split("-");
+                        out.writeObject(dataBase.askForItem(Integer.parseInt(tk[1])));
+                    } catch (Exception e) {
+                        out.writeObject("Oops, qualcosa è andato storto!");
+                    }
+                }else if (richiesta.equals("end")) {
+                    break;
                 } else {
                     out.writeObject("[Debug rete/ClientHandler] said: Errore - Comando non riconosciuto");
                 }
             }
+            dataBase.closeConnection();
 
         } catch (IOException e) {
             System.err.println("Errore di comunicazione con il client: " + e.getMessage());
         } catch (SQLException e) {
             System.err.println("Errore database durante la gestione del client: " + e.getMessage());
+        } catch (NullPointerException e) {
+            System.err.println("Il client ha terminato la connessione in modo anomalo: "+e.getMessage());
         }
 
         System.out.println("Connessione con " + clientSocket.getInetAddress() + " terminata.");
