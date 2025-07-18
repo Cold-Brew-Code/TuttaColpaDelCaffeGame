1c158b1dc295bf95900ceabc0386dfacf8367c56
Giovanni Vendola <giovannivendola87@gmail.com>
2025-07-18 17:16:26 +0200

    ho modificato alcune righe di codice poichè non erano corrette o non gestivano eventuali errori (ese riga 162). Ho iniziato anche a itrudurre anche il cmabio di descrizione della stanza se l'oggetto viene raccolto

--- adventure/src/main/java/it/tutta/colpa/del/caffe/adventure/control/PickUpObserver.java
+++ adventure/src/main/java/it/tutta/colpa/del/caffe/adventure/control/PickUpObserver.java
@@ -8,21 +8,22 @@ package it.tutta.colpa.del.caffe.adventure.control;
  *
  * @author giova
  */
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
-
+import it.tutta.colpa.del.caffe.game.control.ServerInterface;
 import it.tutta.colpa.del.caffe.game.entity.GameDescription;
 import it.tutta.colpa.del.caffe.game.entity.GameObserver;
+import it.tutta.colpa.del.caffe.game.entity.GeneralItem;
 import it.tutta.colpa.del.caffe.game.entity.Inventory;
+import it.tutta.colpa.del.caffe.game.entity.ItemContainer;
 import it.tutta.colpa.del.caffe.game.exception.InventoryException;
+import it.tutta.colpa.del.caffe.game.exception.ServerCommunicationException;
 import it.tutta.colpa.del.caffe.game.utility.CommandType;
 import it.tutta.colpa.del.caffe.game.utility.GameUtils;
 import it.tutta.colpa.del.caffe.game.utility.ParserOutput;
-import java.util.Map;
-import it.tutta.colpa.del.caffe.game.entity.ItemContainer;
-import it.tutta.colpa.del.caffe.game.entity.GeneralItem;
-
-import java.util.ArrayList;
-import java.util.List;
+import static it.tutta.colpa.del.caffe.game.utility.RequestType.UPDATED_LOOK;
 
 /**
  *
@@ -37,130 +38,162 @@ public class PickUpObserver implements GameObserver {
      * @return
      */
     @Override
-    public String update(GameDescription description, ParserOutput parserOutput) {
-    StringBuilder msg = new StringBuilder();
-    Object obj = parserOutput.getObject();
-    boolean conteiner = false;
+    public String update(GameDescription description, ParserOutput parserOutput, ServerInterface server) {
+        StringBuilder msg = new StringBuilder();
+        Object obj = parserOutput.getObject();
+        boolean conteiner = false;
 
-    if (parserOutput.getCommand().getType() == CommandType.PICK_UP) {
-        if (obj == null) {
-            msg.append("Non hai specificato l'oggetto da raccogliere. (scrivi 'raccogli nome oggetto')");
-            return msg.toString();
-            
-        } else if (!description.getCurrentRoom().getObjects().isEmpty()) {
-            GeneralItem isobjRoom = description.getCurrentRoom().getObjects().keySet().stream()
-                .filter(o -> o.getName().equals(parserOutput.getObject().getName())
+        if (parserOutput.getCommand().getType() == CommandType.PICK_UP) {
+            if (obj == null) {
+                msg.append("Non hai specificato l'oggetto da raccogliere. (scrivi 'raccogli nome oggetto')");
+                return msg.toString();
+
+            } else if (!description.getCurrentRoom().getObjects().isEmpty()) {
+                GeneralItem isobjRoom = description.getCurrentRoom().getObjects().keySet().stream()
+                        .filter(o -> o.getName().equals(parserOutput.getObject().getName())
                         || o.getAlias().contains(parserOutput.getObject().getName()))
-                .findFirst()
-                .orElse(null);
+                        .findFirst()
+                        .orElse(null);
 
-            if (isobjRoom == null) {
-                if (description.getCurrentRoom().getObject(7) != null) {
-                    isobjRoom = description.getCurrentRoom().getObject(7);
-                    
-                    conteiner = true;
-                } else if (description.getCurrentRoom().getObject(11) != null) {
-                    isobjRoom = description.getCurrentRoom().getObject(11);
-                    conteiner = true;
-                } else if (description.getCurrentRoom().getObject(15) != null) {
-                    isobjRoom = description.getCurrentRoom().getObject(15);
-                    conteiner = true;
+                if (isobjRoom == null) {
+                    if (description.getCurrentRoom().getObject(7) != null) {
+                        isobjRoom = description.getCurrentRoom().getObject(7);
+                        conteiner = true;
+                    } else if (description.getCurrentRoom().getObject(11) != null) {
+                        isobjRoom = description.getCurrentRoom().getObject(11);
+                        conteiner = true;
+                    } else if (description.getCurrentRoom().getObject(15) != null) {
+                        isobjRoom = description.getCurrentRoom().getObject(15);
+                        conteiner = true;
+                    }
+                    // xkè per controllare all'interno dell'inventario deve prima raccoglierlo o aperto ( per l'armadietto) 
+                    if (isobjRoom instanceof ItemContainer isobjRoomC) {
+                        if (conteiner && isobjRoom.getId() != 15) {
+                            msg.append("l'oggetto non e nella stanza. C'è l'oggetto: ").append(isobjRoomC.getName())
+                                    .append("potrebbe essere al suo interno. ma devi prima raccoglierolo . (Usa il comando prendi nome oggetto");
+                            // cambio la descrizione 
+                            if (isobjRoomC.getId() == 7) {
+                                try {
+                                    // ho raccolto il borsellino quindi cambio la descrizione della stanza 13
+                                    description.getCurrentRoom().setLook(server.requestToServer(UPDATED_LOOK, 7));
+                                } catch (ServerCommunicationException ex) {
+                                }
+                            } else {
+                                try {
+                                    // ho raccolto la scatola quindi cambio la descrizione della stanza 19
+                                    description.getCurrentRoom().setLook(server.requestToServer(UPDATED_LOOK, 9));
+                                } catch (ServerCommunicationException ex) {
+                                }
+                            }
+                            // mentre se l'oggetto non può essere raccolto ma può essere aperto  
+                        } else {
+                            if (isobjRoomC.isOpen()) {
+                                Map.Entry<GeneralItem, Integer> contenuto = isobjRoomC.getList().entrySet().stream()
+                                        .filter(entry -> entry.getKey().getName().equals(parserOutput.getObject().getName())
+                                        || entry.getKey().getAlias().contains(parserOutput.getObject().getName()))
+                                        .findFirst()
+                                        .orElse(null);
+                                if (contenuto != null) {
+                                    GeneralItem objCont = contenuto.getKey();
+                                    int quantity = contenuto.getValue();
+                                    try {
+                                        // Aggiungiamo all'inventario con la stessa quantità
+                                        description.getInventory().add(objCont, quantity);
+                                        msg.append(" ").append(quantity).append(" x ").append(objCont.getName());
+                                        isobjRoomC.remove(objCont, quantity);
+                                        // ho raccolto la candeggina quindi cambio la descrizione della stanza 9
+                                    description.getCurrentRoom().setLook(server.requestToServer(UPDATED_LOOK, 2));
+                                    } catch (InventoryException e) {
+                                        msg.append(" Non puoi aggiungere ")
+                                                .append(objCont.getName())
+                                                .append(" all'inventario: ").append(e.getMessage());
+                                    } catch (IllegalArgumentException e) {
+                                        msg.append(e.getMessage());
+                                    } catch (ServerCommunicationException ex) {
+                                    }
+                                }
+                            } else { // contenitore chiuso
+                                msg.append("Ops l'oggetto : ").append(isobjRoomC.getName())
+                                        .append(" è chiuso. Dovresti prima aprirlo. (Usa il comando APRI nome oggetto");
+                            }
+                        }
+                    }
+                } else { // oggetto trovato direttamente nella stanza
+                    Map<GeneralItem, Integer> objRoom = description.getCurrentRoom().getObjects();
+                    int quantity = objRoom.get(isobjRoom);
+                    try {
+                        description.getInventory().add(isobjRoom, quantity);
+                        msg.append(" ").append(quantity).append(" x ").append(isobjRoom.getName());
+                        objRoom.remove(isobjRoom, quantity);
+                        // oggetto trovato nella stanza quindi è stato raccolto e aggiorno la descrizione della stanza 
+                        description.getCurrentRoom().setLook(server.requestToServer(UPDATED_LOOK, 7));// evento borsellino 
+                    } catch (InventoryException e) {
+                        msg.append(" Non puoi aggiungere ").append(isobjRoom.getName()).append(" all'inventario: ").append(e.getMessage());
+                    } catch (IllegalArgumentException e) {
+                        msg.append(e.getMessage());
+                    } catch (ServerCommunicationException e) {
+                        msg.append(e.getMessage());
+                    }
                 }
 
-                if (conteiner) {
-                    if (isobjRoom instanceof ItemContainer) {
-                        ItemContainer isobjRoomC = (ItemContainer) isobjRoom;
-                        if (isobjRoomC.isOpen()) {
-                            Map.Entry<GeneralItem, Integer> contenuto = isobjRoomC.getList().entrySet().stream()
-                                .filter(entry -> entry.getKey().getName().equals(parserOutput.getObject().getName())
-                                        || entry.getKey().getAlias().contains(parserOutput.getObject().getName()))
-                                .findFirst()
-                                .orElse(null);
-                            if (contenuto != null) {
-                                GeneralItem objCont = contenuto.getKey();
-                                int quantity = contenuto.getValue();
+                // caso inventario     
+            } else if (!description.getInventory().getInventory().isEmpty()) {
+                GeneralItem invObj = (GeneralItem) obj;
+                List<GeneralItem> listContainer = new ArrayList<>();
+                boolean trovato = false;
+                if (!description.getInventory().getInventory().containsKey(invObj)) {
+                    Inventory inventario = description.getInventory();
+                    if (GameUtils.getObjectFromInventory(inventario, 7) != null) {
+                        listContainer.add(GameUtils.getObjectFromInventory(inventario, 7));
+                    }
+                    if (GameUtils.getObjectFromInventory(inventario, 11) != null) {
+                        listContainer.add(GameUtils.getObjectFromInventory(inventario, 11));
+                    }
+                    if (GameUtils.getObjectFromInventory(inventario, 15) != null) {
+                        listContainer.add(GameUtils.getObjectFromInventory(inventario, 15));
+                    }
+
+                    if (!listContainer.isEmpty()) {
+                        for (GeneralItem objCont : listContainer) {
+                            ItemContainer container = (ItemContainer) objCont;
+                            if (container.isOpen()) {
                                 try {
-                                    // Aggiungiamo all'inventario con la stessa quantità
-                                    description.getInventory().add(objCont, quantity);
-                                    msg.append(" ").append(quantity).append(" x ").append(objCont.getName());
-                                    isobjRoomC.remove(objCont, quantity);
+                                    Map<GeneralItem, Integer> pippo = container.getObject(invObj);
+                                    if (pippo != null && !pippo.isEmpty()) {// se il container ha l'oggetto indicato
+                                        GeneralItem objFinde = pippo.keySet().iterator().next();
+                                        int quantity = pippo.get(objFinde);
+                                        description.getInventory().add(objFinde, quantity);
+                                        msg.append(" ").append(quantity).append(" x ").append(objFinde.getName());
+                                        container.remove(objFinde, quantity);
+                                        trovato = true;
+                                        break;
+                                    }
                                 } catch (InventoryException e) {
-                                    msg.append(" Non puoi aggiungere ")
-                                       .append(objCont.getName())
-                                       .append(" all'inventario: ").append(e.getMessage());
+                                    msg.append(" Non puoi aggiungere l'oggetto all'inventario. ").append(e.getMessage());
                                 } catch (IllegalArgumentException e) {
                                     msg.append(e.getMessage());
                                 }
-                            }
-                        } else { // contenitore chiuso
-                            msg.append("Ops l'oggetto : ").append(isobjRoomC.getName())
-                               .append(" è chiuso. Dovresti prima aprirlo. (Usa il comando APRI nome oggetto");
-                        }
-                    }
-                }
-            } else { // oggetto trovato direttamente nella stanza
-                Map<GeneralItem, Integer> objRoom = description.getCurrentRoom().getObjects();
-                int quantity = objRoom.get(isobjRoom);
-                try {
-                    description.getInventory().add(isobjRoom, quantity);
-                    msg.append(" ").append(quantity).append(" x ").append(isobjRoom.getName());
-                    objRoom.remove(isobjRoom, quantity);
-                } catch (InventoryException e) {
-                    msg.append(" Non puoi aggiungere ").append(isobjRoom.getName()).append(" all'inventario: ").append(e.getMessage());
-                } catch (IllegalArgumentException e) {
-                    msg.append(e.getMessage());
-                }
-            }
-        } else if (!description.getInventory().getInventory().isEmpty()) {
-            GeneralItem invObj = (GeneralItem) obj;
-            List<GeneralItem> listContainer = new ArrayList<>();
-            boolean trovato = false;
-            if (!description.getInventory().getInventory().containsKey(invObj)) {
-                Inventory inventario = description.getInventory();
-                if (GameUtils.getObjectFromInventory(inventario, 7) != null) {
-                    listContainer.add(GameUtils.getObjectFromInventory(inventario, 7));
-                }
-                if (GameUtils.getObjectFromInventory(inventario, 11) != null) {
-                    listContainer.add(GameUtils.getObjectFromInventory(inventario, 11));
-                }
-                if (GameUtils.getObjectFromInventory(inventario, 15) != null) {
-                    listContainer.add(GameUtils.getObjectFromInventory(inventario, 15));
-                }
+                            } else {
+                                // contenitore chiuso
+                                msg.append("Ops l'oggetto : ").append(container.getName())
+                                        .append(" è chiuso. Dovresti prima aprirlo. (Usa il comando APRI nome oggetto");
 
-                if (!listContainer.isEmpty()) {
-                    for (GeneralItem objCont : listContainer) {
-                        ItemContainer container = (ItemContainer) objCont;
-                        if (container.isOpen()) {
-                            try {
-                                Map<GeneralItem, Integer> pippo = container.getObject(invObj);
-                                GeneralItem objFinde = pippo.keySet().iterator().next();
-                                int quantity = pippo.get(objFinde);
-                                description.getInventory().add(objFinde, quantity);
-                                msg.append(" ").append(quantity).append(" x ").append(objFinde.getName());
-                                container.remove(objFinde, quantity);
-                                trovato = true;
-                                break;
-                            } catch (InventoryException e) {
-                                msg.append(" Non puoi aggiungere l'oggetto all'inventario. ").append(e.getMessage());
-                            } catch (IllegalArgumentException e) {
-                                msg.append(e.getMessage());
                             }
                         }
                     }
+                    if (!trovato || listContainer.isEmpty()) {
+                        msg.append("L'oggetto ").append(invObj.getName()).append(" non è nell'inventario");
+                    }
+                } else {
+                    msg.append("Hai già l'oggetto nell'inventario, non prendi nulla.");
                 }
-                if (!trovato || listContainer.isEmpty()) {
-                    msg.append("L'oggetto ").append(invObj.getName()).append(" non è nell'inventario");
-                }
+            } else if (description.getInventory().getInventory().isEmpty()) {
+
+                msg.append("Impossibile, non hai oggetti nell'inventario.");
             } else {
-                msg.append("Hai già l'oggetto nell'inventario, non prendi nulla.");
-            }
-        } else if (description.getInventory().getInventory().isEmpty()) {
-            
-            msg.append("Impossibile, non hai oggetti nell'inventario.");
-            } else {
-            
-            msg.append("Nella stanza: ").append(description.getCurrentRoom().getName())
-               .append(" non è presente l'oggetto ").append(parserOutput.getObject().getName());
+
+                msg.append("Nella stanza: ").append(description.getCurrentRoom().getName())
+                        .append(" non è presente l'oggetto ").append(parserOutput.getObject().getName());
             }
         }
         return msg.toString();
