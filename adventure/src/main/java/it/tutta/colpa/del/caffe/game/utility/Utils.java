fae689451a36d36d3da4b163c29b8e1f3967a7a0
Giovanni Vendola <giovannivendola87@gmail.com>
2025-08-20 16:47:52 +0200

    javaDoc

--- adventure/src/main/java/it/tutta/colpa/del/caffe/game/utility/Utils.java
+++ adventure/src/main/java/it/tutta/colpa/del/caffe/game/utility/Utils.java
@@ -15,16 +15,25 @@ import java.util.List;
 import java.util.Set;
 
 /**
+ * Classe che contenente metodi statici per operazioni su file e
+ * stringhe. Fornisce funzionalità di caricamento di liste da file e parsing di
+ * stringhe con rimozione di stopwords.
  *
- * @author pierpaolo
+ * @author giova
  */
 public class Utils {
 
     /**
+     * Carica il contenuto di un file in un {@link Set} di stringhe.
+     * <p>
+     * Ogni riga del file viene letta, convertita in minuscolo e aggiunta al
+     * set. Le righe duplicate saranno automaticamente eliminate grazie alla
+     * natura del {@link Set}.
+     * </p>
      *
-     * @param file
-     * @return
-     * @throws IOException
+     * @param file il file da leggere
+     * @return un {@link Set} contenente tutte le righe del file in minuscolo
+     * @throws IOException se si verifica un errore durante la lettura del file
      */
     public static Set<String> loadFileListInSet(File file) throws IOException {
         Set<String> set = new HashSet<>();
@@ -37,10 +46,16 @@ public class Utils {
     }
 
     /**
+     * Parsea una stringa in token, rimuovendo eventuali stopwords.
+     * <p>
+     * La stringa viene convertita in minuscolo e divisa in parole utilizzando
+     * gli spazi come separatori. Tutte le parole presenti nell'insieme
+     * {@code stopwords} vengono scartate.
+     * </p>
      *
-     * @param string
-     * @param stopwords
-     * @return
+     * @param string la stringa da parsare
+     * @param stopwords un {@link Set} contenente le parole da ignorare
+     * @return una {@link List} di token filtrati, mantenendo l'ordine originale
      */
     public static List<String> parseString(String string, Set<String> stopwords) {
         List<String> tokens = new ArrayList<>();
@@ -52,5 +67,4 @@ public class Utils {
         }
         return tokens;
     }
-
 }
