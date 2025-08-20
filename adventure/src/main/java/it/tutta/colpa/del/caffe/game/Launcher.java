e6bebb945dfbf0990d50e4fcd96199050d786ba9
Giovanni Vendola <giovannivendola87@gmail.com>
2025-08-20 16:36:47 +0200

    javaDoc

--- adventure/src/main/java/it/tutta/colpa/del/caffe/game/Launcher.java
+++ adventure/src/main/java/it/tutta/colpa/del/caffe/game/Launcher.java
@@ -6,18 +6,37 @@ import it.tutta.colpa.del.caffe.game.control.Controller;
 import it.tutta.colpa.del.caffe.game.control.Engine;
 import it.tutta.colpa.del.caffe.start.control.MainPageController;
 
-import javax.swing.*;
-
 /**
  * @author giovav
- * @since 14/07/25
+ * @since 14/07/25 La classe {@code Launcher} si occupa di avviare la schermata
+ * di gioco principale.
+ * <p>
+ * Quando viene istanziata, chiude la GUI iniziale (menu principale) e apre la
+ * schermata di gioco {@link GamePage}, collegandola al controller
+ * {@link Engine}.
+ * </p>
  */
 public class Launcher {
-    public Launcher(MainPageController mainPageController){
+
+    /**
+     * Costruttore della classe {@code Launcher}.
+     * <p>
+     * - Chiude la GUI iniziale tramite il controller passato come parametro.
+     * <br>
+     * - Crea una nuova istanza di {@link GamePage}. <br>
+     * - Inizializza un nuovo {@link Engine} come controller per la pagina di
+     * gioco. <br>
+     * - Collega la pagina di gioco al controller appena creato.
+     * </p>
+     *
+     * @param mainPageController il controller della pagina principale (menu
+     * iniziale), usato per chiudere la GUI e inizializzare il nuovo engine
+     */
+    public Launcher(MainPageController mainPageController) {
         mainPageController.closeGUI();
 
         GameGUI bo = new GamePage();
-        Controller controller=new Engine(mainPageController, bo);
+        Controller controller = new Engine(mainPageController, bo);
         controller.toString();
         bo.linkController(controller);
     }
