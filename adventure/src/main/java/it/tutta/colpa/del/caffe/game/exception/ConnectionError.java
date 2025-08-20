cb3ddfee9579c29b5bfab3e3a23619b16a17c30c
Giovanni Vendola <giovannivendola87@gmail.com>
2025-08-20 16:57:37 +0200

    javaDOC

--- adventure/src/main/java/it/tutta/colpa/del/caffe/game/exception/ConnectionError.java
+++ adventure/src/main/java/it/tutta/colpa/del/caffe/game/exception/ConnectionError.java
@@ -1,13 +1,25 @@
 package it.tutta.colpa.del.caffe.game.exception;
 
-
-
 /**
+ * Eccezione personalizzata che viene lanciata quando si verifica un errore di
+ * connessione, ad esempio durante richieste HTTP a servizi esterni.
+ * <p>
+ * Estende {@link Exception}, quindi è un'eccezione controllata.
+ * </p>
  *
  * @author giova
  */
 public class ConnectionError extends Exception {
+
+    /**
+     * Costruisce una nuova ConnectionError con un messaggio descrittivo e la
+     * causa dell'errore.
+     *
+     * @param msg il messaggio che descrive l'errore di connessione
+     * @param cause la causa originale dell'eccezione
+     */
     public ConnectionError(String msg, Throwable cause) {
         super(msg, cause);
     }
+
 }
