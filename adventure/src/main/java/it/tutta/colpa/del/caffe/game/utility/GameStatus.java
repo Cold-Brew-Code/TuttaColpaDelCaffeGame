957457468c67788575eaab82c274e90d1cd4e4c4
giovav <giovannivittore@icloud.com>
2025-08-21 12:53:42 +0200

    Modificati i possibili stati che il gioco può assumere.

--- adventure/src/main/java/it/tutta/colpa/del/caffe/game/utility/GameStatus.java
+++ adventure/src/main/java/it/tutta/colpa/del/caffe/game/utility/GameStatus.java
@@ -3,6 +3,8 @@ package it.tutta.colpa.del.caffe.game.utility;
 public enum GameStatus {
     IN_CORSO,
     BAGNO_USATO,
-    PERSA,
-    VINTA
+    BOCCIATO,
+    PROMOSSO,
+    BAGNO_USATO_TEMPO_ESAURITO,
+    TEMPO_ESAURITO
 }
